50d738f9dc7e19845df02c3ac1467fda1c5e43da
Scott Schreckengaust <scott@schrecktech.com>
2014-06-08 15:52:06 -0700

    Adding how to get the data, subset and make first plot.

---
+++ getting.R
@@ -0,0 +1,10 @@
+rm(list=ls())
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp, method="curl")
+unzip(temp) 
+unlink(temp)
+library(data.table)
+hpc <- read.table(file="household_power_consumption.txt", sep=";", na.strings="?", header=TRUE)
+hpc$Date <- as.Date(strptime(hpc$Date, format="%d/%m/%Y"))
+hpc <- hpc[hpc$Date >= '2007-02-01', ]
+hpc <- hpc[hpc$Date < '2007-02-03', ]
---
+++ plot1.R
@@ -0,0 +1,3 @@
+png(filename="plot1.png", width=480, height=480)
+hist(x=hpc$Global_active_power, xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power", col="red")
+dev.off()
